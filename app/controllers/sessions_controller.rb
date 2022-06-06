a6f08276fea785b8d0380e5879ba7cf3ce78e69b
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-06-06 18:05:46 +0000

    Save3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,19 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Welcome."
         redirect_to "/places"
       else
-        flash["notice"] = "Nope."
-        redirect_to "/login"
+        redirect_to "/sessions/new"
       end
     else
-      flash["notice"] = "Nope."
-      redirect_to "/login"
+      redirect_to "/sessions/new"
     end
   end
 
   def destroy
     session["user_id"] = nil
     flash["notice"] = "Goodbye."
-    redirect_to "/login"
+    redirect_to "/sessions/new"
   end
 end
   
