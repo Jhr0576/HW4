2371c24a6d8a5759ece5e7450f5a7bbe45eddd06
Jhr0576 <jacob.rice@kellogg.northwestern.edu>
2022-06-06 19:57:03 +0000

    Save11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  before_action :set_current_user
 
-  def current_user
+  def set_current_user
     puts "------------------ code before every request ------------------"
-    
+
     @current_user = User.find_by({ "id" => session["user_id"] })
   end
 end
